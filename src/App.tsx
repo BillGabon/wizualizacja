f61cc0cab70892f9a5047a0c35017de591d5d822
Jakub <jakub.kwiatek@virtual.com>
2024-05-21 23:39:45 +0200

    komit dziadu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import { useState, useRef } from "react";
 import { AnimatePresence, Reorder, motion } from 'framer-motion';
 import ChartWrapper from './ChartWrapper';
+import { Button, Card, TextInput } from '@tremor/react';
+import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
 
 function App() {
   const count = useRef(0);
@@ -11,8 +13,37 @@ function App() {
 
   return (
     <>
+<div className="w-full p-4 mb-1 flex flex-row-reverse">
+<Popover>
+  <PopoverTrigger asChild><Button size='xs' variant='primary' className='px-4 py-2 mx-1'>Register</Button></PopoverTrigger>
+  <PopoverContent>
+    <Card className='mt-2'>
+    <TextInput type='email' placeholder="email" className='my-2'></TextInput>
+    <TextInput type='password' className='my-2' placeholder='password'></TextInput>
+    <div className='mb-1 flex flex-row-reverse'><Button size='xs' variant='primary' className='px-4 py-1'>Register</Button>
+    </div>
+    </Card>
+  </PopoverContent>
+</Popover>
+
+<Popover>
+  <PopoverTrigger asChild><Button size='xs' variant='primary' className='px-4 py-2 mx-2'>Login</Button></PopoverTrigger>
+  <PopoverContent>
+    <Card className='mt-2'>
+    <TextInput type='email' placeholder="email" className='my-2'></TextInput>
+    <TextInput type='password' className='my-2' placeholder='password'></TextInput>
+    <div className='mb-1 flex flex-row-reverse'><Button size='xs' variant='primary' className='px-4 py-2'>Login</Button></div>
+    </Card>
+  </PopoverContent>
+</Popover>
+
+<Button size='xs' variant='primary' className='px-4 py-2' color='red'>Logout</Button>
+</div>
+
+
+    
       <div className='justify-center flex'>
-        <motion.button className='rounded-md bg-rose-500 text-white p-3 self-center'
+        <motion.button className='rounded-md bg-rose-500 text-white p-3 self-center mb-3'
           whileTap={{ scale: 0.95 }}
           onClick={() => {
             count.current++;
